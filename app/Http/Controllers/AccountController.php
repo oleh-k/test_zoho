c5df103106e74d06447fb99415caa1a5b33d76b2
oleh_ok <oleh.k@hotmail.com>
2023-04-17 22:04:59 +0300

    back: added api route and controller - route resource '/accounts' - create account

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use Illuminate\Http\Request;
+
+class AccountController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+
+        $account = Account::create($request->all());
+
+        $response = [
+            "success" => true,
+            "account_id" => $account->id,
+            "message" => "New account successfully registered"
+        ];
+
+        return response($response, 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\ZohoTokensController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/grand_token', [ZohoTokensController::class, 'getRefreshToken']);
 
 Route::get('/refresh_token', [ZohoTokensController::class, 'getAccessToken']);
+
+Route::resource('/accounts', AccountController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
