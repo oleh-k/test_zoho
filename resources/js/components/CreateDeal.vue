93bc1a7fc82f83307126d6b9eacb7fc01e6f6ebf
oleh_ok <oleh.k@hotmail.com>
2023-04-20 16:58:57 +0300

    front: show list of account in Create Deal form

--- resources/js/components/CreateDeal.vue
+++ resources/js/components/CreateDeal.vue
@@ -8,6 +8,13 @@
             <input type="text" v-model="stage" id="stage" placeholder="stage">
         </div>
         <div>
+            <select name="account" id="account" v-model="account">
+                <option v-for="account in accounts" :value="account.id">
+                    {{ account.name }}
+                </option>
+            </select>
+        </div>
+        <div>
             <button @click="createDeal()" class="button">submit</button>
         </div>
         <div>
@@ -23,7 +30,9 @@ export default {
         return {
             name: '',
             stage: '',
+            account: '',
             result: '',
+            accounts: []
 
         }
     },
@@ -34,6 +43,7 @@ export default {
             const formData = new FormData();
             formData.append('name', this.name);
             formData.append('stage', this.stage);
+            formData.append('account', this.account);
 
             const headers = { 
                 'Content-Type': 'multipart/form-data',
@@ -48,12 +58,23 @@ export default {
             })
 
 
+        },
+
+        getAccounts () {
+
+            axios.get('api/accounts').then((res) => {
+                console.log(res.data)
+                this.accounts = res.data.data
+            }).catch((res) => {
+                console.log(res)
+            })
+
         }
 
 
     },
     mounted() {
-
+        this.getAccounts();
     }
 }
 </script>
